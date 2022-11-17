1859a2107845f8e989c1e7227e6861c94d037a80
Saba-Var <vartasashvili94@gmail.com>
2022-11-17 12:10:33 +0400

    feat: translate 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,8 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { LottieAnimation } from 'components'
+import type { GetStaticProps } from 'next'
 import { PageNotFount } from 'public'
+import { useTranslate } from 'hooks'
 import Head from 'next/head'
 import Link from 'next/link'
 
@@ -11,7 +14,7 @@ const NotFound = () => {
         <meta
           content='initial-scale=1.0, width=device-width'
           name='viewport'
-          key='Not Found'
+          key='404'
         />
       </Head>
 
@@ -23,18 +26,17 @@ const NotFound = () => {
         <div className='flex flex-col gap-4 mx-8'>
           <div>
             <h2 className='text-4xl text-center font-bold tracking-tight text-gray-500 sm:text-5xl'>
-              Page not found
+              {useTranslate('404:title')}
             </h2>
 
             <p className='mt-3 sm:mt-5 text-xl sm:text-2xl text-center text-gray-500'>
-              The page you are looking for might have been removed or is
-              temporarily unavailable.
+              {useTranslate('404:description')}
             </p>
           </div>
 
           <Link href='/overview'>
             <a className='hover:underline mt-1 text-center text-xl text-gray-500 font-medium'>
-              &larr; Go back to home
+              &larr; {useTranslate('404:link-text')}
             </a>
           </Link>
         </div>
@@ -44,3 +46,11 @@ const NotFound = () => {
 }
 
 export default NotFound
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, ['404'])),
+    },
+  }
+}
