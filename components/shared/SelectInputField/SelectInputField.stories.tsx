0ea67a6a05627e5ca7ad46404d4a39b4cb5aea01
Saba-Var <vartasashvili94@gmail.com>
2023-08-24 16:35:58 +0400

    story: select input field

---
+++ components/shared/SelectInputField/SelectInputField.stories.tsx
@@ -0,0 +1,62 @@
+import { SelectInputField, StoryFormTemplate } from 'components'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof SelectInputField> = {
+  component: SelectInputField,
+  title: 'Core/SelectInputField',
+  tags: ['autodocs'],
+  args: {
+    name: 'select',
+    title: 'Choose subject',
+    optionsList: [
+      {
+        id: 1,
+        title: 'Math',
+      },
+      {
+        id: 2,
+        title: 'History',
+      },
+      {
+        id: 3,
+        title: 'English',
+      },
+    ],
+  },
+  argTypes: {},
+  decorators: [
+    (Story, { args: { name } }) => (
+      <StoryFormTemplate inputName={name}>
+        <Story />
+      </StoryFormTemplate>
+    ),
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<typeof SelectInputField>
+
+export const Basic: Story = {}
+
+export const Optional: Story = {
+  args: {
+    required: false,
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const Error: Story = {
+  decorators: [
+    (Story, { args: { name } }) => (
+      <StoryFormTemplate generateError inputName={name}>
+        <Story />
+      </StoryFormTemplate>
+    ),
+  ],
+}
