c288517f0c1b7de71bcf69e13d07be1fdaae21a8
Saba-Var <vartasashvili94@gmail.com>
2023-08-24 12:37:32 +0400

    story: input error message

---
+++ components/shared/InputErrorMessage/InputErrorMessage.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { InputErrorMessage } from 'components'
+import { FieldError } from 'react-hook-form'
+
+const meta: Meta<typeof InputErrorMessage> = {
+  component: InputErrorMessage,
+  title: 'Core/InputErrorMessage',
+  tags: ['autodocs'],
+  parameters: { layout: 'centered' },
+  args: {
+    errorMessage: {
+      message: 'This is an error message' as unknown as FieldError,
+    },
+  },
+  argTypes: {
+    errorMessage: {
+      description: 'The error message to display',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof InputErrorMessage>
+
+export const Basic: Story = {}
